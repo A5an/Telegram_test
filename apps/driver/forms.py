b6c059d0852b1953c7e7ffd1ccdef17039813460
Aukenov Assanali <115568332+AsanX99999@users.noreply.github.com>
2023-09-20 21:44:24 +0600

    comb4

--- apps/driver/forms.py
+++ apps/driver/forms.py
@@ -4,5 +4,5 @@ from apps.client.models import Lobby
 class LobbyForm(ModelForm):
     class Meta:
         model = Lobby
-        fields = ['host','availableSeats', 'currentLoc_lat', 'currentLoc_lng', 'arrivalLoc_lat', 'arrivalLoc_lng']
+        fields = ['host','availableSeats']
     
