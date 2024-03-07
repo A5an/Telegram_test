87ce8e4bd98410947a064e5cb3088c89ac409842
Aukenov Assanali <115568332+AsanX99999@users.noreply.github.com>
2024-03-08 00:33:58 +0600

    hhee first

--- apps/client/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . models import *
-
-
-admin.site.register(Lobby)
-# Register your models here
--- apps/driver/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from apps.client.models import Lobby
-
-class LobbyForm(ModelForm):
-    class Meta:
-        model = Lobby
-        fields = ['host','availableSeats', 'arrivalLoc']
-    
