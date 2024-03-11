7e6146c703827b65ec72a7aefc198a049b35f39a
Aukenov Assanali <115568332+AsanX99999@users.noreply.github.com>
2024-03-11 21:16:54 +0500

    ura

--- index.js
+++ index.js
@@ -82,6 +82,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+let bots = {};
 
 let count = 0;
 // Роут для добавления нового бота
@@ -91,11 +92,16 @@ app.post('/addBot', async (req, res) => {
         return res.status(400).json({ error: 'Bot token is required.' });
     }
 
+    if (bots[newBotToken]) {
+        return res.status(409).json({ error: 'Bot with this token already exists.' });
+    }
+
     try {
         count++;
         // Создаем новый экземпляр бота
         const bot = new TelegramBot(newBotToken, { polling: true });
-
+        const botId = newBotToken // Функция для генерации уникального идентификатора
+        bots[botId] = bot;
         bot.setMyCommands([
                 {command: '/start', description: 'Welcome message Easy life'},
                 {command: '/info', description: 'Information about bot'}
@@ -106,8 +112,23 @@ app.post('/addBot', async (req, res) => {
             const text = msg.text;
             const chatId = msg.chat.id;
         
+            // if (msg.photo) {
+            //     // Если сообщение содержит фото, отправить ответ "Это фото"
+            //     return bot.sendMessage(chatId, 'Это фото');
+            // }
+        
+            // if (msg.audio) {
+            //     // Если сообщение содержит аудио, отправить ответ "Это аудио"
+            //     return bot.sendMessage(chatId, 'Это аудио');
+            // }
+
+            // if (msg.voice) {
+            //     // Если сообщение содержит аудио, отправить ответ "Это аудио"
+            //     return bot.sendMessage(chatId, 'Это голос');
+            // }
+
             if (text === '/start') {
-               return bot.sendMessage(chatId, `Welcome to pleep ${count}`, {
+               return bot.sendMessage(chatId, `Welcome to pleep ${count, newBotToken} `, {
                 reply_markup: {
                     inline_keyboard: [
                         [{text: 'Pleep', web_app: {url: 'https://pleep.app//chat/ac0bb767-2949-4038-955d-0b637ea114bc'}}],
@@ -115,6 +136,7 @@ app.post('/addBot', async (req, res) => {
                 }
                });
             }
+            
             if (text === '/info') {
                 return bot.sendMessage(chatId, `Pleep is universal chatbot ${msg.from.first_name}!`);
             }
@@ -127,6 +149,26 @@ app.post('/addBot', async (req, res) => {
     } catch (error) {
         console.error('Error adding new bot:', error);
         return res.status(500).json({ error: 'Error adding new bot.' });
+    }
+});
+
+
+
+app.delete('/stopBot/:botId', (req, res) => {
+    const botId = req.params.botId;
+    const bot = bots[botId];
+
+    if (!bot) {
+        return res.status(404).json({ error: 'Bot not found.' });
+    }
+
+    if (bot) {
+        bot.removeAllListeners();
+        bot.stopPolling(); // Останавливаем бота
+        delete bots[botId]; // Удаляем бота из объекта
+        res.status(200).json({ message: `Bot ${botId} stopped successfully.` });
+    } else {
+        res.status(404).json({ error: 'Bot not found.' });
     }
 });
 
